2a84614ea3b9816b9ea0dbcb2c85a9b4e5bb190c
jacneeley <jacneeley@outlook.com>
2025-11-21 15:52:00 -0600

    more repo tests

--- src/test/java/simplepayroll_public/repo/JobsiteRepoTests.java
+++ src/test/java/simplepayroll_public/repo/JobsiteRepoTests.java
@@ -1,13 +1,118 @@
 package simplepayroll_public.repo;
 
+import java.time.LocalDate;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import local.payrollapp.simplepayroll.components.MockDatabase;
+import local.payrollapp.simplepayroll.jobsite.Jobsite;
+import local.payrollapp.simplepayroll.jobsite.JobsiteRepo;
 
 @ExtendWith(MockitoExtension.class)
 public class JobsiteRepoTests {
 	@Mock
 	private MockDatabase mockDb = new MockDatabase();
+	
+	@Autowired
+	private JobsiteRepo jobsiteRepo = new JobsiteRepo(mockDb);
+	
+	@Test
+	public void createJobsite_ReturnJobsite() {
+		Long generatedId = 1000L + (long) (Math.random() * (10000L - 1000L));
+		Jobsite jobsite = new Jobsite(
+				generatedId.toString(),
+				"LINDSEY",
+				Boolean.FALSE,
+				Boolean.TRUE,
+				LocalDate.now(),
+				LocalDate.now());
+		
+		jobsiteRepo.CreateJobsite(jobsite);
+		Assertions.assertThat(jobsite).isNotNull();
+		Assertions.assertThat(jobsiteRepo.findJobsiteById(jobsite.getId(), Boolean.TRUE).get()).isNotNull();
+	}
+	
+	@Test
+	public void findAllActiveJobsites() {
+		Long generatedId = 1000L + (long) (Math.random() * (10000L - 1000L));
+		Jobsite jobsite = new Jobsite(
+				generatedId.toString(),
+				"LINDSEY",
+				Boolean.FALSE,
+				Boolean.TRUE,
+				LocalDate.now(),
+				LocalDate.now());
+		jobsiteRepo.CreateJobsite(jobsite);
+		
+		generatedId = 1000L + (long) (Math.random() * (10000L - 1000L));
+		Jobsite jobsite1 = new Jobsite(
+				generatedId.toString(),
+				"NATHAN",
+				Boolean.FALSE,
+				Boolean.FALSE,
+				LocalDate.now(),
+				LocalDate.now());
+		jobsiteRepo.CreateJobsite(jobsite1);
+		
+		generatedId = 1000L + (long) (Math.random() * (10000L - 1000L));
+		Jobsite jobsite2 = new Jobsite(
+				generatedId.toString(),
+				"TEXAN",
+				Boolean.FALSE,
+				Boolean.TRUE,
+				LocalDate.now(),
+				LocalDate.now());
+		jobsiteRepo.CreateJobsite(jobsite2);
+		
+		Assertions.assertThat(jobsiteRepo.findAllJobsitesByActive(Boolean.TRUE).size() == 2).isTrue();
+	}
+	
+	@Test
+	public void jobsiteSoftDeleteAndUpdate() {
+		Long generatedId = 1000L + (long) (Math.random() * (10000L - 1000L));
+		Jobsite jobsite = new Jobsite(
+				generatedId.toString(),
+				"LINDSEY",
+				Boolean.FALSE,
+				Boolean.TRUE,
+				LocalDate.now(),
+				LocalDate.now());
+		jobsiteRepo.CreateJobsite(jobsite);
+		Assertions.assertThat(jobsite).isNotNull();
+		Assertions.assertThat(jobsiteRepo.findJobsiteById(jobsite.getId(), Boolean.TRUE).get()).isNotNull();
+		
+		Jobsite updatedJobsite = new Jobsite(
+				jobsite.getId(),
+				jobsite.getJobsiteName(),
+				jobsite.isContract(),
+				Boolean.FALSE,
+				jobsite.getCreatedAt(),
+				LocalDate.now());
+		jobsiteRepo.UpdateJobsite(updatedJobsite, updatedJobsite.getId());
+		Assertions.assertThat(jobsiteRepo.findJobsiteById(updatedJobsite.getId(), Boolean.FALSE).get().getId().equals(generatedId.toString())).isTrue();
+		Assertions.assertThat(jobsiteRepo.findAllJobsitesByActive(Boolean.FALSE).size() == 1).isTrue();
+	}
+	
+	@Test
+	public void jobsiteDeletion() {
+		Long generatedId = 1000L + (long) (Math.random() * (10000L - 1000L));
+		Jobsite jobsite = new Jobsite(
+				generatedId.toString(),
+				"LINDSEY",
+				Boolean.FALSE,
+				Boolean.TRUE,
+				LocalDate.now(),
+				LocalDate.now());
+		jobsiteRepo.CreateJobsite(jobsite);
+		Assertions.assertThat(jobsite).isNotNull();
+		Assertions.assertThat(jobsiteRepo.findJobsiteById(jobsite.getId(), Boolean.TRUE).get()).isNotNull();
+		
+		jobsiteRepo.DeleteJobsite(jobsite.getId());
+		Assertions.assertThat(jobsiteRepo.findJobsiteById(jobsite.getId(), Boolean.TRUE).isEmpty()).isTrue();
+	}
 }
